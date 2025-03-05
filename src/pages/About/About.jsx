c23056eb949e1d48cfb962c8cd701c88164738d5
ELHATIMISOUFIANE <soufianeelhatimi@gmail.com>
2025-03-05 17:13:05 +0100

    last commit

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import images from "../../constant/images";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaGitAlt, FaLaravel, FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import { SiSpring } from "react-icons/si";
+
+export const About = () => {
+  return (
+    <div
+      id="about"
+      className="mt-[15vh] flex flex-col gap-y-[5vh] text-white p-10">
+      <div className="flex flex-col lg:flex-row items-center px-[5vw] lg:px-[8vw] justify-between gap-10">
+        <motion.div
+          variants={fadeIn("right", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-col gap-y-4 w-full lg:w-[40vw]">
+          <h1 className="text-4xl lg:text-6xl">About Me</h1>
+          <p className="text-lg lg:text-2xl">
+            Hi I’m Walid Cadi, a full-stack web developer with a strong focus on
+            building web applications. I have expertise in both front-end and
+            back-end technologies, allowing me to develop seamless user
+            experiences from design to deployment.
+          </p>
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="w-full lg:w-auto">
+          <img className="w-full lg:w-auto" src={images.dall} alt="" />
+        </motion.div>
+      </div>
+
+      <div className="flex mt-[15vh] flex-col items-center gap-y-7">
+        <motion.h1
+          variants={fadeIn("down", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="text-3xl lg:text-4xl font-bold">
+          Skills
+        </motion.h1>
+        <motion.div
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-wrap justify-center items-center gap-5 lg:gap-x-10">
+          <div className="bg-transparent border-2 border-white p-5 lg:p-7 w-[45vw] sm:w-[25vw] lg:w-[15vw] rounded-xl flex flex-col items-center gap-y-2 ">
+            <div className="flex flex-col items-center hover:scale-110 duration-300">
+              <IoLogoJavascript className="text-white text-6xl lg:text-7xl" />
+              <h1 className="text-white text-lg lg:text-xl cursor-default font-semibold">
+                JavaScript
+              </h1>
+            </div>
+          </div>
+
+          <div className="bg-transparent border-2 border-white p-5 lg:p-7 w-[45vw] sm:w-[25vw] lg:w-[15vw] rounded-xl flex flex-col items-center gap-y-2 ">
+            <div className="flex flex-col items-center hover:scale-110 duration-300">
+              <FaReact className="text-white text-6xl lg:text-7xl" />
+              <h1 className="text-white text-lg lg:text-xl cursor-default font-semibold">
+                React
+              </h1>
+            </div>
+          </div>
+
+          <div className="bg-transparent border-2 border-white p-5 lg:p-7 w-[45vw] sm:w-[25vw] lg:w-[15vw] rounded-xl flex flex-col items-center gap-y-2 ">
+            <div className="flex flex-col items-center hover:scale-110 duration-300">
+              <FaLaravel className="text-white text-6xl lg:text-7xl" />
+              <h1 className="text-white text-lg lg:text-xl cursor-default font-semibold">
+                Laravel
+              </h1>
+            </div>
+          </div>
+          <div className="bg-transparent border-2 border-white p-5 lg:p-7 w-[45vw] sm:w-[25vw] lg:w-[15vw] rounded-xl flex flex-col items-center gap-y-2 ">
+            <div className="flex flex-col items-center hover:scale-110 duration-300">
+              <SiSpring className="text-white text-6xl lg:text-7xl" />
+              <h1 className="text-white text-lg lg:text-xl cursor-default font-semibold">
+                Spring
+              </h1>
+            </div>
+          </div>
+
+
+          <div className="bg-transparent border-2 border-white p-5 lg:p-7 w-[45vw] sm:w-[25vw] lg:w-[15vw] rounded-xl flex flex-col items-center gap-y-2 ">
+            <div className="flex flex-col items-center hover:scale-110 duration-300">
+              <RiTailwindCssFill className="text-white text-6xl lg:text-7xl" />
+              <h1 className="text-white text-lg lg:text-xl cursor-default font-semibold">
+                Tailwind CSS
+              </h1>
+            </div>
+          </div>
+
+          <div className="bg-transparent border-2 border-white p-5 lg:p-7 w-[45vw] sm:w-[25vw] lg:w-[15vw] rounded-xl flex flex-col items-center gap-y-2 ">
+            <div className="flex flex-col items-center hover:scale-110 duration-300">
+              <FaGitAlt className="text-white text-6xl lg:text-7xl" />
+              <h1 className="text-white text-lg lg:text-xl cursor-default font-semibold">
+                Git
+              </h1>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,186 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { fadeIn } from "../../variants";
+import { motion } from "framer-motion";
+
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_58d5q6p", "template_0e3wodx", form.current, {
+        publicKey: "HnsFyZCXu9-nkZFWG",
+      })
+      .then(
+        () => {
+          alert("SUCCESS!");
+        },
+        (error) => {
+          alert("FAILED...", error.text);
+        }
+      );
+  };
+  return (
+    // <div
+    //   id="contact"
+    //   className="flex flex-col items-center gap-y-5 text-white p-10">
+    //   <h1 className="text-4xl font-bold">Contact Me</h1>
+    //   <div className="flex items-start gap-x-4">
+    //     <form
+    //       className="flex flex-col gap-2 p-6 bg-transparent border-2 border-white rounded-xl"
+    //       ref={form}
+    //       onSubmit={sendEmail}>
+    //       <input
+    //         className="bg-transparent border-2 border-white rounded-lg p-5 focus:outline-none"
+    //         required
+    //         type="text"
+    //         name="name"
+    //         placeholder="Name..."
+    //       />
+    //       <div className="flex gap-3 items-center">
+    //         <input
+    //           className="bg-transparent border-2 border-white rounded-lg p-5 focus:outline-none"
+    //           required
+    //           type="email"
+    //           name="user_email"
+    //           placeholder="Email..."
+    //         />
+    //         <input
+    //           className="bg-transparent border-2 border-white rounded-lg p-5 focus:outline-none"
+    //           required
+    //           type="text"
+    //           name="user_phone"
+    //           placeholder="Phone..."
+    //         />
+    //       </div>
+    //       <textarea
+    //         className="bg-transparent border-2 border-white rounded-lg p-5 focus:outline-none"
+    //         required
+    //         name="message"
+    //         placeholder="Message"
+    //         rows={6}
+    //       />
+    //       <button
+    //         type="submit"
+    //         value="Send"
+    //         class="mb-6 inline-block w-full border-2 border-white rounded-lg bg-transparent px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-[#8E05C2]">
+    //         Send
+    //       </button>
+    //     </form>
+    //     <div className="flex flex-col gap-y-5">
+    //       <a
+    //         target="_blank"
+    //         href="https://www.linkedin.com/in/walid-cadi-532b38236/"
+    //         className="overflow-hidden flex items-center justify-center bg-transparent border-2 border-white rounded-full w-[100px] h-[100px] ">
+    //         <FaLinkedinIn className="text-6xl text-white hover:scale-110 duration-300" />
+    //       </a>
+    //       <a
+    //         target="_blank"
+    //         href="https://www.instagram.com/ca_waliiid/"
+    //         className="overflow-hidden flex items-center justify-center bg-transparent border-2 border-white rounded-full w-[100px] h-[100px] ">
+    //         <FaInstagram className="text-6xl text-white hover:scale-110 duration-300" />
+    //       </a>
+    //       <a
+    //         target="_blank"
+    //         href=""
+    //         className="overflow-hidden flex items-center justify-center bg-transparent border-2 border-white rounded-full w-[100px] h-[100px] ">
+    //         <SiGmail className="text-6xl text-white hover:scale-110 duration-300" />
+    //       </a>
+    //     </div>
+    //   </div>
+    // </div>
+    <div
+      id="contact"
+      className="mt-[15vh] flex flex-col items-center gap-y-5 text-white p-5 lg:p-10">
+      <motion.h1
+        variants={fadeIn("down", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.7 }}
+        className="text-3xl lg:text-4xl font-bold">
+        Contact Me
+      </motion.h1>
+
+      {/* Contact form and social links container */}
+      <div className="flex flex-col lg:flex-row items-start gap-5 lg:gap-x-10 w-full lg:w-auto">
+        {/* Contact Form */}
+        <motion.form
+          variants={fadeIn("right", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-col gap-2 p-6 bg-transparent border-2 border-white rounded-xl w-full lg:w-auto"
+          ref={form}
+          onSubmit={sendEmail}>
+          <input
+            className="bg-transparent border-2 border-white rounded-lg p-4 lg:p-5 focus:outline-none w-full"
+            required
+            type="text"
+            name="name"
+            placeholder="Name..."
+          />
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-3 items-center">
+            <input
+              className="bg-transparent border-2 border-white rounded-lg p-4 lg:p-5 focus:outline-none w-full"
+              required
+              type="email"
+              name="user_email"
+              placeholder="Email..."
+            />
+            <input
+              className="bg-transparent border-2 border-white rounded-lg p-4 lg:p-5 focus:outline-none w-full"
+              required
+              type="text"
+              name="user_phone"
+              placeholder="Phone..."
+            />
+          </div>
+          <textarea
+            className="bg-transparent border-2 border-white rounded-lg p-4 lg:p-5 focus:outline-none w-full"
+            required
+            name="message"
+            placeholder="Message"
+            rows={6}
+          />
+          <button
+            type="submit"
+            value="Send"
+            className="mt-4 inline-block w-full border-2 border-white rounded-lg bg-transparent px-6 py-2.5 font-medium uppercase leading-normal text-white hover:shadow-md hover:bg-[#631f7e]">
+            Send
+          </button>
+        </motion.form>
+
+        {/* Social Media Icons */}
+        <motion.div
+          variants={fadeIn("left", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex items-center gap-x-7 lg:flex-col gap-y-10">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/soufiane-el-hatimi-550927297/"
+            className="overflow-hidden flex items-center justify-center bg-transparent border-2 border-white rounded-full w-[80px] h-[80px] lg:w-[100px] lg:h-[100px]">
+            <FaLinkedinIn className="text-4xl lg:text-6xl text-white hover:scale-110 duration-300" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.instagram.com/soufiane_elhatimi/"
+            className="overflow-hidden flex items-center justify-center bg-transparent border-2 border-white rounded-full w-[80px] h-[80px] lg:w-[100px] lg:h-[100px]">
+            <FaInstagram className="text-4xl lg:text-6xl text-white hover:scale-110 duration-300" />
+          </a>
+          <a
+            target="_blank"
+            href=""
+            className="overflow-hidden flex items-center justify-center bg-transparent border-2 border-white rounded-full w-[80px] h-[80px] lg:w-[100px] lg:h-[100px]">
+            <SiGmail className="text-4xl lg:text-6xl text-white hover:scale-110 duration-300" />
+          </a>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
